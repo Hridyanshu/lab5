74149297e10c1f29ce5a1ddc4f899b7ad8847f52
Hridyanshu <43311666+Hridyanshu@users.noreply.github.com>
2019-02-14 10:56:10 +0530

    Update file1.cpp

--- file1.cpp
+++ file1.cpp
@@ -8,10 +8,13 @@ class student
 };
 int main()
 {
-	student obj;
-	obj.name="XYZ";
-	obj.roll_no=123;
-	cout<<obj.name<<endl<<obj.roll_no<<endl;
+	student obj1;
+	student obj2;
+	obj1.name="XYZ";
+	obj1.roll_no=123;
+	obj2.name="PQR";
+	obj2.roll_no=456;
+	cout<<obj1.name<<endl<<obj1.roll_no<<endl<<obj2.name<<endl<<obj2.roll_no;
 	return 0;
 }
 
